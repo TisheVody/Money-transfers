b241f6d5570fad63c0d8cbc87b3a0ed9311f58c8
TisheVody <trust97@icloud.com>
2022-02-07 23:41:32 +0300

    add Meloman

--- src/main/kotlin/People.kt
+++ src/main/kotlin/People.kt
@@ -1,6 +1,6 @@
-package `2`
 
-    fun main() {
+
+fun main() {
         val likes = 1211211
         val end = if ((likes % 10 == 1) && (likes % 100 !==11)) "человеку" else "людям"
         println("Понравилось $likes $end")
